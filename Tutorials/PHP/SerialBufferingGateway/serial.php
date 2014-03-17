7496f514d38fabc46b58ecab6233ee99c2b0c18f
Trystan Lea <trystan.lea@googlemail.com>
2014-03-17 12:55:35 +0000

    Update serial.php

--- Tutorials/PHP/SerialBufferingGateway/serial.php
+++ Tutorials/PHP/SerialBufferingGateway/serial.php
@@ -3,17 +3,34 @@
 $fp = fopen("importlock", "w");
 if (! flock($fp, LOCK_EX | LOCK_NB)) { echo "Already running\n"; die; }
 
-$host = "http://emoncms.org";
+// Remote server settings
 
-// Enter your emoncms write api key here:
-$apikey = "";
+    // Host name of the remote server
+    $host = "http://emoncms.org";
 
-$time = microtime(true);
-$lastrxtime = $time;
-$lastsendtime = $time;
-$lastsendsuccess = $time;
+    // Remote server emoncms api key (read & write) found in input api helper tab
+    $apikey = "";
 
-$buffer = "[";
+    // Connection timeout - both should be set to less than buffer_send_interval
+    // The number of seconds to wait while trying to connect.
+    $connect_timeout = 3;
+    // The maximum number of seconds to allow cURL functions to execute.
+    $total_timeout = 3;
+    
+
+// Buffer settings
+
+    // Maximum number of packets that can be sent in one request to emoncms
+    // distributes load on server in the event of mass simultaneous upload from many monitors
+    // Balance between availability of internet and max load.
+    $max_upload_lines = 500;
+
+    // Minimum interval for sending data to emoncms
+    // if internet has been down for hours and several buffer blocks (of max_upload_lines) are full
+    // this will set the time inbetween each sent block
+    $buffer_send_interval = 5;
+    
+
 
 // Read from serial with data coming from RFM12PI with RFM12_Demo sketch
 // All Emoncms code is released under the GNU Affero General Public License.
@@ -43,23 +60,34 @@ if (PATH_SEPARATOR != ";") {
 $f = fopen($filename, "r+", false, $c);
 stream_set_timeout($f, 0,1000);
 
-$lastsend = time();
 
+// Buffer vars
+$buffer = array();
+$buffer[0] = "";
+$bcount = 0;
+
+$start_time = time();
+$buffertime = microtime(true);
+
+$val = 0;
 while(true)
 {
-    $time = microtime(true);
+    
+    $timeindex = time()-$start_time;
+    $microtime = microtime(true);
 
+    // On serial
     if ($instr = fgets($f))
     {
         $instr = trim($instr);
-
+        
         // Split input string
         $parts = explode(' ',$instr);
 
         if ($instr!="" && count($parts))
         {
             // Start of packet description
-            $packet = array(time()-$lastsend,0);
+            $packet = array($timeindex,0);
 
             // Type validation
             for ($i=0; $i<count($parts); $i++)
@@ -67,27 +95,59 @@ while(true)
                 $packet[] = (float) $parts[$i];
             }
 
-            if ($buffer!="[") $buffer.=",";
-            $buffer .= json_encode($packet);
-
-            print "Request ";
-            print memory_get_usage()." ".memory_get_usage(true)." ";
-            //print $buffer."]";
-            $response = request($host,"POST","/input/bulk.json",$apikey,$buffer."]");
-
-            if ($response=='ok') {
-                print "ok\n";
-                $lastsend = time();
-                $buffer = "[";
-            } else {
-                print "failed\n";
+            $bi = count($buffer)-1;
+            // The buffer is stored as a precompiled json string ready for sending to the remote server
+            if ($buffer[$bi]!="") $buffer[$bi].=",";
+            $buffer[$bi] .= json_encode($packet); 
+            
+            $bcount ++;
+            
+            // Segments the buffer into blocks in order to limit the max upload size to the server
+            // if too many monitors try and upload a lot of data simultaneously the server may be
+            // unable to handle it.
+            if ($bcount>=$max_upload_lines) {
+              $buffer[] = "";
+              $bcount = 0;
             }
+        }
+        
+        // Verbose output of buffer
+        // print "-------------------------------\n";
+        // for ($b=0; $b<count($buffer); $b++)
+        // {
+        // print $b.": ".$buffer[$b]."\n";
+        // }
+    }
+    
 
+    
+    // If there is data in the buffer try sending every 1s
+    if ($buffer[0]!="" && ($microtime-$buffertime)>=$buffer_send_interval)
+    {
+        $buffertime = $microtime;
+        
+        print "Sending ";
+        // print "Sending: "."[".$buffer[0]."]&sentat=".$timeindex."\n";
+        $response = request($host,"POST","/input/bulk.json",$apikey,"data=[".$buffer[0]."]&sentat=".$timeindex);
+        
+        if ($response=='ok')
+        {
+            print "ok\n";
+            if (count($buffer)>1) {
+                array_shift($buffer);
+            } else {
+                $buffer[0] = "";
+                $bcount = 0;
+            }
+        } 
+        else
+        {
+            print "failed\n";
         }
     }
     
     // Sleep for a little while so that script doesnt loop to fast
-    usleep(250000);
+    usleep(10000);
 }
 
 fclose($f);
@@ -99,13 +159,13 @@ function request($host,$method,$path,$apikey,$data)
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     //curl_setopt($curl, CURLOPT_HEADER, true);
     curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
-    curl_setopt($curl, CURLOPT_POSTFIELDS,"data=".$data);
+    curl_setopt($curl, CURLOPT_POSTFIELDS,$data);
     
     // Timeouts - max time to wait before pronouncing request failure
-    curl_setopt($curl, CURLOPT_CONNECTTIMEOUT,3);
-    curl_setopt($curl, CURLOPT_TIMEOUT, 3);
+    global $connect_timeout, $total_timeout;
+    curl_setopt($curl, CURLOPT_CONNECTTIMEOUT,$connect_timeout);
+    curl_setopt($curl, CURLOPT_TIMEOUT,$total_timeout);
     $curl_response = curl_exec($curl);
     curl_close($curl);
     return $curl_response;
 }
-
