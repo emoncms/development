546269d27ad0b70e16007be352b92a462dffe3ec
Trystan Lea <trystan.lea@googlemail.com>
2015-02-24 21:59:31 +0000

    Update myip_controller.php

--- Modules/myip/myip_controller.php
+++ Modules/myip/myip_controller.php
@@ -14,7 +14,7 @@
 
   function myip_controller()
   {
-    global $mysqli,$session, $route;
+    global $mysqli,$session, $route, $redis;
 
     $result = false;
 
@@ -32,6 +32,9 @@
         $userid = $session['userid'];
         $result = $mysqli->query("SELECT ipaddress FROM myip WHERE userid = '$userid';");
         $row = $result->fetch_object();
+        
+        $row->time = (time() - $redis->get("myip:$userid:time"));
+        $row->lanip = $redis->get("myip:$userid:lanip");
         $result = $row;
       }
 
@@ -44,6 +47,17 @@
         $parts = explode(".",$ip);
         foreach ($parts as $part) $part = (int) $part;
         $ip = implode(".",$parts);
+        
+        if (isset($_GET['lanip'])){
+            $parts = explode(".",$_GET['lanip']);
+            foreach ($parts as $part) $part = (int) $part;
+            $lanip = implode(".",$parts);
+            
+            $redis->set("myip:$userid:lanip",$lanip);
+        }
+        
+        $time = time();
+        $redis->set("myip:$userid:time",$time);
         
         // Delete old ip
         $mysqli->query("DELETE FROM myip WHERE `userid`='$userid'");
